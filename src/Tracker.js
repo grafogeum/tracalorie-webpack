2ff804a75423d33ad9f0e757beb4048df191a545
michal <grafogeum@users.noreply.github.com>
2023-09-04 17:46:17 +0200

    Initial commit

---
+++ src/Tracker.js
@@ -0,0 +1,205 @@
+import Storage from "./Storage";
+
+class CaloriesTracker {
+	constructor() {
+		this._caloriesLimit = Storage.getCalorieLimit();
+		this._totalCalories = Storage.getTotalCalories(0);
+		this._meals = Storage.getMeals();
+		this._workouts = Storage.getWorkouts();
+
+		this._displayCaloriesLimits();
+		this._displayCaloriesTotal();
+		this._displayCaloriesConsumed();
+		this._displayCaloriesBurned();
+		this._displayCaloriesRemaining();
+		this._displayCaloriesProgress();
+
+		document.getElementById("limit").value = this._caloriesLimit;
+	}
+
+	// Public API
+	addMeal(meal) {
+		this._meals?.push(meal);
+		this._totalCalories += meal.calories;
+		Storage.updateTotalCalories(this._totalCalories);
+		Storage.saveMeal(meal);
+		this._displayNewMeal(meal);
+		this._render();
+	}
+	addWorkout(workout) {
+		this._workouts?.push(workout);
+		this._totalCalories -= workout.calories;
+		Storage.updateTotalCalories(this._totalCalories);
+		Storage.saveWorkout(workout);
+		this._displayNewWorkout(workout);
+		this._render();
+	}
+
+	removeMeal(id) {
+		const index = this._meals.findIndex((meal) => meal.id === id);
+		if (index === -1) {
+			return;
+		}
+
+		const meal = this._meals[index];
+		this._totalCalories -= meal.calories;
+		Storage.updateTotalCalories(this._totalCalories);
+		this._meals.splice(index, 1);
+		Storage.removeMeal(id);
+		this._render();
+	}
+
+	removeWorkout(id) {
+		const index = this._workouts.findIndex((workout) => workout.id === id);
+		if (index === -1) {
+			return;
+		}
+
+		const workout = this._workouts[index];
+		this._totalCalories += workout.calories;
+		Storage.updateTotalCalories(this._totalCalories);
+		this._workouts.splice(index, 1);
+		Storage.removeWorkout(id);
+		this._render();
+	}
+
+	reset() {
+		this._totalCalories = 0;
+		this._meals = [];
+		this._workouts = [];
+		Storage.clearAll();
+		this._render();
+	}
+
+	setLimit(calorieLimit) {
+		this._caloriesLimit = calorieLimit;
+		Storage.setCalorieLimit(calorieLimit);
+		this._displayCaloriesLimits();
+		this._render();
+	}
+
+	loadItems() {
+		this._meals.forEach((meal) => this._displayNewMeal(meal));
+		this._workouts.forEach((workout) => this._displayNewWorkout(workout));
+	}
+
+	// Private API
+	_displayCaloriesTotal() {
+		const totalCaloriesEl = document.getElementById("calories-total");
+		totalCaloriesEl.innerText = this._totalCalories;
+	}
+	_displayCaloriesLimits() {
+		const calorieLimitEl = document.getElementById("calories-limit");
+		calorieLimitEl.innerText = this._caloriesLimit;
+	}
+
+	_displayCaloriesConsumed() {
+		const caloriesConsumedEl = document.getElementById("calories-consumed");
+		const consumed = this._meals?.reduce(
+			(acc, { calories }) => acc + calories,
+			0
+		);
+
+		caloriesConsumedEl.innerText = consumed;
+	}
+
+	_displayCaloriesBurned() {
+		const caloriesBurnedEl = document.getElementById("calories-burned");
+		const burned = this._workouts?.reduce(
+			(acc, { calories }) => acc + calories,
+			0
+		);
+
+		caloriesBurnedEl.innerText = 0;
+		caloriesBurnedEl.innerText = burned;
+	}
+
+	_displayCaloriesRemaining() {
+		const caloriesRemainingEl = document.getElementById("calories-remaining");
+		const remaining = this._caloriesLimit - this._totalCalories;
+		caloriesRemainingEl.innerText = remaining;
+
+		const progressEl = document.getElementById("calorie-progress");
+
+		if (remaining <= 0) {
+			caloriesRemainingEl.parentElement.parentElement.classList.remove(
+				"bg-light"
+			);
+			caloriesRemainingEl.parentElement.parentElement.classList.add(
+				"bg-danger"
+			);
+			progressEl.classList.remove("bg-success");
+			progressEl.classList.add("bg-danger");
+		} else {
+			caloriesRemainingEl.parentElement.parentElement.classList.remove(
+				"bg-danger"
+			);
+			caloriesRemainingEl.parentElement.parentElement.classList.add("bg-light");
+			progressEl.classList.remove("bg-danger");
+			progressEl.classList.add("bg-success");
+		}
+	}
+
+	_displayCaloriesProgress() {
+		const progressEl = document.getElementById("calorie-progress");
+		const percentage = (this._totalCalories / this._caloriesLimit) * 100;
+		const width = Math.min(percentage, 100);
+		progressEl.style.width = `${width}%`;
+	}
+
+	_displayNewMeal(meal) {
+		const mealsEl = document.getElementById("meal-items");
+		const mealEl = document.createElement("div");
+		mealEl.classList.add("card", "my-2");
+		mealEl.setAttribute("data-id", meal.id);
+		mealEl.innerHTML = `
+		<div class="card-body">
+			<div class="d-flex align-items-center justify-content-between">
+				<h4 class="mx-1">${meal.name}</h4>
+				<div
+					class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+				>
+					${meal.calories}
+				</div>
+				<button class="delete btn btn-danger btn-sm mx-2">
+					<i class="fa-solid fa-xmark"></i>
+				</button>
+			</div>
+		</div>
+	`;
+		mealsEl.appendChild(mealEl);
+	}
+
+	_displayNewWorkout(workout) {
+		const workoutsEl = document.getElementById("workout-items");
+		const workoutEl = document.createElement("div");
+		workoutEl.classList.add("card", "my-2");
+		workoutEl.setAttribute("data-id", workout.id);
+		workoutEl.innerHTML = `
+		<div class="card-body">
+			<div class="d-flex align-items-center justify-content-between">
+				<h4 class="mx-1">${workout.name}</h4>
+				<div
+					class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+				>
+					${workout.calories}
+				</div>
+				<button class="delete btn btn-danger btn-sm mx-2">
+					<i class="fa-solid fa-xmark"></i>
+				</button>
+			</div>
+		</div>
+	`;
+		workoutsEl.appendChild(workoutEl);
+	}
+
+	_render() {
+		this._displayCaloriesTotal();
+		this._displayCaloriesConsumed();
+		this._displayCaloriesBurned();
+		this._displayCaloriesRemaining();
+		this._displayCaloriesProgress();
+	}
+}
+
+export default CaloriesTracker;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: "./src/app.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "app.js",
+	},
+	// stats: "verbose",
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "dist"),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: / node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Webpack Configured this Title",
+			filename: "index.html",
+			template: "./src/index.html",
+		}),
+		new MiniCssExtractPlugin(),
+	],
+};
